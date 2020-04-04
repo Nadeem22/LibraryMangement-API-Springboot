0957bd90edc6001d195e3f43d8f80a6f7319e4e8
Nadeem22 <nadeem2107@gmail.com>
2020-04-04 11:37:31 +0530

    Unit testing is done for Library api

---
+++ src/test/java/com/nadeem/api/libraryapis/testutils/TestConstants.java
@@ -0,0 +1,10 @@
+package com.nadeem.api.libraryapis.testutils;
+
+public class TestConstants {
+    public static final String API_TRACE_ID="Test-trace-id";
+    public static final String TEST_PUBLISHER_NAME="TestPublisherNAme";
+    public static final String TEST_PUBLISHER_EMAIL="TestPublisherEmail@gmail.com";
+    public static final String TEST_PUBLISHER_PHONE="111-222-333";
+    public static final String TEST_PUBLISHER_EMAIL_UPDATED="TestPublisherNew@gmail.com";
+    public static final String TEST_PUBLISHER_PHONE_UPDATED="333-222-333";
+}
